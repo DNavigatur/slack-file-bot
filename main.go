b2be1717e1679acd48d39902bd3149b07687a9e0
DNavigatur <98771560+DNavigatur@users.noreply.github.com>
2023-10-08 17:45:11 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5374522069330-6006340342643-kzLgE8VITILg1V3IiWHvNW7G")
+	os.Setenv("CHANNEL_ID", "C05AXH45KQV")
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"win.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+
+}
